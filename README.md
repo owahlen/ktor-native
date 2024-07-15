abe3c35865fca96e2588aab99db838c7b6930b2a
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2024-07-15 18:15:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# embeddedServer - Native
+
+A sample project demonstrating how to run a Ktor server in a Kotlin/Native application. Open [build.gradle.kts](build.gradle.kts) to see the Gradle configuration.
+> This sample is a part of the [codeSnippets](../../README.md) Gradle project.
+
+## Running
+
+To run a sample, execute the following command in a repository's root directory:
+```bash
+./gradlew :embedded-server-native:runReleaseExecutableNative
+```
+
+To test this sample, execute the `nativeTest` task:
+
+```bash
+./gradlew :embedded-server-native:nativeTest
+```
---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+val ktorVersion: String = "2.3.12"
+
+plugins {
+    application
+    kotlin("multiplatform") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val arch = System.getProperty("os.arch")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && arch == "x86_64" -> macosX64("native")
+        hostOs == "Mac OS X" && arch == "aarch64" -> macosArm64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        // Other supported targets are listed here: https://ktor.io/docs/native-server.html#targets
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    sourceSets {
+        val nativeMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-core:$ktorVersion")
+                implementation("io.ktor:ktor-server-cio:$ktorVersion")
+            }
+        }
+        val nativeTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.ktor:ktor-server-test-host:$ktorVersion")
+            }
+        }
+    }
+}
